f93ba97b1288cc65194261a746c05518aaf070ff
montillha <marianaalmeida235b@gmail.com>
2024-05-20 15:32:35 -0300

    projeto de gerenciamento de conta bancaria

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        // TODO: conhecer e importar o classe scaner,
+        // exibir as msg
+        // obter pela classe Scanner os valores digitados pelo terminal
+        // exibie msg cionta criada
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o nome do cliente:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o nome da agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco,sua agência é "
+                + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+    }
+}
